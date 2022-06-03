60c92e7521ea7ab942326046bb2860c162c595fe
Jean Paulo Athanazio De Mei <93294014+Jean-Paulo-Public@users.noreply.github.com>
2022-06-02 23:32:14 -0300

    # 1 # Melhoria no salvamento da anotação
    Não funcionou por motivo desconhecido,
    porém abondonei por que está rápido salvar direto.
    
    Type
      TCallBack = Reference to procedure;
      TUnidade = (ms, s, m, h);
    
      ...
    
          SetTimeOut(Procedure
                     begin
                       SaveKey(self.name + '.Text');
                     end, 5,s );
    
      ...
    
    procedure TAnotacoesDockableForm.SetTimeOut(CallBack: TCallBack; Tempo: Double; Unidade: TUnidade);
    var vInterval: Integer;
        Timer: Cardinal;
    begin
      vInterval := 0;
      case Unidade of
        ms: vInterval := Round(Tempo);
        s: vInterval := Round(Tempo * 1000);
        m: vInterval := Round(Tempo * 60000);
        h: vInterval := Round(Tempo * 3600000);
      end;
    
      SetTimer(0, 0, vInterval,
      @procedure
      begin
        CallBack;
      end);
    end;
    
    ...

--- Anotacoes/AnotacoesDockForm.pas
+++ Anotacoes/AnotacoesDockForm.pas
@@ -8,13 +8,16 @@ uses
   FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
   FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
   FireDAC.Comp.DataSet, FireDAC.Comp.Client, Vcl.StdCtrls, Vcl.Mask, JvExMask,
-  JvToolEdit, JvBaseEdits, JvDBControls, System.Win.Registry;
+  JvToolEdit, JvBaseEdits, JvDBControls, System.Win.Registry, System.Threading;
 
 type
   TAnotacoesDockableForm = class(TBaseDockableForm)
     Memo1: TMemo;
     procedure FormActivate(Sender: TObject);
     procedure Memo1KeyPress(Sender: TObject; var Key: Char);
+  private
+    Saving: Boolean;
+    procedure SaveKey(KeyName, KeyValue: string);
   public
     class procedure RemoveAnotacoesDockForm;
     class procedure ShowAnotacoesDockForm;
@@ -31,6 +34,22 @@ var FormInstance: TBaseDockableForm = nil;
 
 { TAnotacoesDockableForm }
 
+procedure TAnotacoesDockableForm.SaveKey(KeyName, KeyValue: string);
+var reg: TRegistry;
+begin
+  reg := TRegistry.Create(KEY_READ);
+  reg.RootKey := HKEY_CURRENT_USER;
+  reg.Access := KEY_WRITE;
+
+  if not reg.OpenKey(cKey,True)
+    then reg.CreateKey(cKey);
+
+  Reg.WriteString(KeyName, KeyValue);
+
+  reg.CloseKey();
+  reg.Free;
+end;
+
 class procedure TAnotacoesDockableForm.CreateAnotacoesDockForm;
 begin
   if not Assigned(FormInstance)
@@ -44,45 +63,26 @@ begin
 end;
 
 procedure TAnotacoesDockableForm.FormActivate(Sender: TObject);
-var
-  lReg: TRegistry;
-  getInt: integer;
+  procedure LoadKey(KeyName: string);
+  var lReg: TRegistry;
+  begin
+    lReg := TRegistry.Create;
+    lReg.RootKey := HKEY_CURRENT_USER;
+    lReg.OpenKey(cKey, True);
+
+    if lReg.ValueExists(KeyName)
+      then Memo1.Text := lReg.ReadString(KeyName);
+
+    lReg.CloseKey;
+    lReg.Free;
+  end;
 begin
-  inherited;
-  lReg := TRegistry.Create;
-  lReg.RootKey := HKEY_CURRENT_USER;
-  lReg.OpenKey(cKey, True);
-
-  if lReg.ValueExists(self.name + '.Text')
-    then Memo1.Text := lReg.ReadString(self.name + '.Text');
-
-  lReg.CloseKey;
-  lReg.Free;
+  LoadKey(self.name + '.Text');
 end;
 
 procedure TAnotacoesDockableForm.Memo1KeyPress(Sender: TObject; var Key: Char);
-var
-  reg        : TRegistry;
-  openResult : Boolean;
 begin
-  inherited;
-  if Key = #13 then begin
-    reg := TRegistry.Create(KEY_READ);
-    reg.RootKey := HKEY_CURRENT_USER;
-
-    reg.Access := KEY_WRITE;
-    openResult := reg.OpenKey(cKey,True);
-
-    if not openResult then begin
-      reg.CreateKey(cKey + '.Text');
-      ShowMessage('Salvo com sucesso! Será salvo sempre ao dar enter');
-    end;
-
-    Reg.WriteString(self.name + '.Text', Memo1.Text);
-
-    reg.CloseKey();
-    reg.Free;
-  end;
+  SaveKey(self.name + '.Text', Memo1.Text + Key);
 end;
 
 class procedure TAnotacoesDockableForm.RemoveAnotacoesDockForm;
